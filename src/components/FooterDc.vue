88ce42100f48448e5b7dba336b83e6344d9a434e
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-06 16:25:33 +0200

    some try about dc logo

--- src/components/FooterDc.vue
+++ src/components/FooterDc.vue
@@ -1,87 +1,99 @@
 <template>
   <footer>
-    <div class="container">
-      <div class="row py-5 justify-content-between">
-        <div
-          class="
-            col-auto
-            d-flex
-            flex-direction-row
-            align-items-center
-            container_footer_images
-          "
-        >
-          <div class="wrapper_footer_images">
-            <img src="../assets/buy-comics-digital-comics.png" alt="" />
-          </div>
-          DIGITAL COMICS
-        </div>
-        <div
-          class="
-            col-auto
-            d-flex
-            flex-direction-row
-            align-items-center
-            container_footer_images
-          "
-        >
-          <div class="wrapper_footer_images">
-            <img src="../assets/buy-comics-merchandise.png" alt="" />
-          </div>
-          DIGITAL COMICS
-        </div>
-        <div
-          class="
-            col-auto
-            d-flex
-            flex-direction-row
-            align-items-center
-            container_footer_images
-          "
-        >
-          <div class="wrapper_footer_images">
-            <img src="../assets/buy-comics-subscriptions.png" alt="" />
-          </div>
-          DIGITAL COMICS
-        </div>
-        <div
-          class="
-            col-auto
-            d-flex
-            flex-direction-row
-            align-items-center
-            container_footer_images
-          "
-        >
-          <div class="wrapper_footer_images">
-            <img src="../assets/buy-comics-shop-locator.png" alt="" />
-          </div>
-          DIGITAL COMICS
-        </div>
-        <div
-          class="
-            col-auto
-            d-flex
-            flex-direction-row
-            align-items-center
-            container_footer_images
-          "
-        >
+    <div class="container_bg_blue">
+      <div class="container">
+        <div class="row py-5 justify-content-between">
           <div
             class="
-              wrapper_footer_images
+              col-auto
               d-flex
               flex-direction-row
               align-items-center
+              container_footer_images
             "
           >
-            <img
-              class="special_image"
-              src="../assets/buy-dc-power-visa.svg"
-              alt=""
-            />
+            <div class="wrapper_footer_images">
+              <img src="../assets/buy-comics-digital-comics.png" alt="" />
+            </div>
+            DIGITAL COMICS
           </div>
-          DIGITAL COMICS
+          <div
+            class="
+              col-auto
+              d-flex
+              flex-direction-row
+              align-items-center
+              container_footer_images
+            "
+          >
+            <div class="wrapper_footer_images">
+              <img src="../assets/buy-comics-merchandise.png" alt="" />
+            </div>
+            DIGITAL COMICS
+          </div>
+          <div
+            class="
+              col-auto
+              d-flex
+              flex-direction-row
+              align-items-center
+              container_footer_images
+            "
+          >
+            <div class="wrapper_footer_images">
+              <img src="../assets/buy-comics-subscriptions.png" alt="" />
+            </div>
+            DIGITAL COMICS
+          </div>
+          <div
+            class="
+              col-auto
+              d-flex
+              flex-direction-row
+              align-items-center
+              container_footer_images
+            "
+          >
+            <div class="wrapper_footer_images">
+              <img src="../assets/buy-comics-shop-locator.png" alt="" />
+            </div>
+            DIGITAL COMICS
+          </div>
+          <div
+            class="
+              col-auto
+              d-flex
+              flex-direction-row
+              align-items-center
+              container_footer_images
+            "
+          >
+            <div
+              class="
+                wrapper_footer_images
+                d-flex
+                flex-direction-row
+                align-items-center
+              "
+            >
+              <img
+                class="special_image"
+                src="../assets/buy-dc-power-visa.svg"
+                alt=""
+              />
+            </div>
+            DIGITAL COMICS
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="jumbo_footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-7"></div>
+          <div class="col-sm-5">
+            <img src="../assets/dc-logo-bg.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
@@ -98,7 +110,7 @@ export default {
 <style scoped lang="scss">
 @import "../variables";
 
-footer {
+.container_bg_blue {
   background-color: $background-color-blue;
 }
 
@@ -113,6 +125,16 @@ footer {
   }
   .special_image {
     height: 65%;
+  }
+}
+.jumbo_footer {
+  background-image: url(../assets/footer-bg.jpg);
+  height: 482px;
+  .col-sm-5 {
+    height: 482px;
+    img {
+      height: 100%;
+    }
   }
 }
 </style>
