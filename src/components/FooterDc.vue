ca885aa4984b02782c1f3854b68d6c611739046d
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-06 15:13:58 +0200

    added empty footer

---
+++ src/components/FooterDc.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <h1>ciaooooooooooooo</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterDc",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
