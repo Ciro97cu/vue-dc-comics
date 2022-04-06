769146e049525a1e7d8c762fc30140708e1102fd
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-06 15:13:45 +0200

    first layout main

---
+++ src/components/MainDc.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="container py-5">
+      <h1>Content goes Here</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainDc",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../variables";
+main {
+  background-color: $background-color-dark;
+  h1 {
+    color: $text-color-white;
+  }
+}
+</style>
