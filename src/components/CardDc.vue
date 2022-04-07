69da440a74b9c2f4da4ac772c4059352bc31b287
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-07 15:20:31 +0200

    push for ticket

---
+++ src/components/CardDc.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card"></div>
+</template>
+
+<script>
+export default {
+  name: "CardItem",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/MainDc.vue
+++ src/components/MainDc.vue
@@ -1,24 +1,48 @@
 <template>
   <main>
-    <div class="container py-5">
-      <h1>Content goes Here</h1>
+    <div class="jumbotron"></div>
+    <div class="container_cards py-5">
+      <div class="container-sm">
+        <div class="row">
+          <div class="cards col-sm-2">
+            <div class="card">
+              <img src="@/assets/img/jumbotron.jpg" alt="" />
+              <p>ACTION COMICS</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
+// import CardItem from "@/components/CardItem.vue";
 export default {
   name: "MainDc",
+  // components: {
+  //   CardItem,
+  // },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "@/assets/style/variables";
+
 main {
-  background-color: $background-color-dark;
-  h1 {
-    color: $text-color-white;
+  .jumbotron {
+    background-image: url(@/assets/img/jumbotron.jpg);
+    background-size: cover;
+    height: 350px;
+  }
+  .container_cards {
+    background-color: $background-color-dark;
+    .cards {
+      img {
+        width: 100%;
+      }
+    }
   }
 }
 </style>
